73c0593de80f8a557d51aa67d3a7b3e1001222d5
stefan.balea <faneatiku@yahoo.com>
2025-11-29 15:20:43 +0200

    Add error message for exceeding 50-ticket contribution limit.

--- src/utils/user_errors.tsx
+++ src/utils/user_errors.tsx
@@ -2,6 +2,7 @@ const errorsMap: { [key: string]: string } = {
     "failed to execute message; message index: 0: amount is smaller than staking reward min stake": "Amount is smaller than minimum required stake",
     "the resulted amount is too low": "Swap minimum amount could not be met. Increase the slippage and try again.",
     "amount is too low to be traded": "Amount is too low to be traded",
+    "can not buy more than 50 tickets": "You can only contribute up to 50 times per transaction.",
 };
 
 export const prettyError = (err: string|undefined): string|undefined => {
