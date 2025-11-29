fe7dbfa9f3ea64006b5c31584fdca4a8de5d5b6e
stefan.balea <faneatiku@yahoo.com>
2025-11-29 15:24:36 +0200

    Update DEFAULT_TX_MEMO to use "burner.getbze.com".

--- src/constants/placeholders.tsx
+++ src/constants/placeholders.tsx
@@ -2,4 +2,4 @@
 export const TOKEN_LOGO_PLACEHOLDER = '/images/token.svg';
 export const BZE_CIRCLE_LOGO = '/images/bze_alternative_512x512.png';
 
-export const DEFAULT_TX_MEMO = 'dex.getbze.com'
+export const DEFAULT_TX_MEMO = 'burner.getbze.com'
